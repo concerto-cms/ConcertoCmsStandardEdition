4b5530738deee7fffe347ef7cd83f668c4615506
Mathieu Maes <mathieu@zimmo.be>
2016-08-15 16:20:34 +0200

    Upgrade to Symfony 2.8

--- app/autoload.php
+++ app/autoload.php
@@ -2,12 +2,9 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
-
 /**
  * @var ClassLoader $loader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
-
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
 return $loader;
